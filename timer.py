c590ea97b46d6849ca70482c35ad9dffa32d9429
Deju Adejo <ojomideju2003@gmail.com>
2024-05-03 18:36:47 +0100

    Complete debugging process

--- timer.py
+++ timer.py
@@ -2,6 +2,10 @@ import tkinter.messagebox
 from tkinter import *
 import time
 
+NO_OF_SESSIONS = 4
+FOCUS_DURATION = 0.05  # In minutes
+BREAK_DURATION = 0.05
+
 
 class Clock:
     def __init__(self):
@@ -21,63 +25,68 @@ class Clock:
         # Create the start button
         self.start_button = Button(master=self.root, text='Start', command=self.start_timer)
         self.start_button.grid(column=0, row=2)
-        # Create the stop button
-        # Initialize in disabled state (timer is static)
-        self.stop_button = Button(master=self.root, text='Stop', state='disabled', command=self.stop_timer)
-        self.stop_button.grid(column=2, row=2)
         # Create the reset button
-        self.reset_button = Button(master=self.root, text='Reset', command=self.reset_timer)
-        self.reset_button.grid(column=1, row=4)
+        self.reset_button = Button(master=self.root, text='Reset', command=self.reset_timer, state='disabled')
+        self.reset_button.grid(column=2, row=2)
+        # Create text to show the current session
+        self.current_session = Label(master=self.root, text='Session: 0', bg='#F7F6BB', fg='#114232',
+                                     font=('Courier', 13, 'bold'))
+        self.current_session.grid(column=1, row=3)
+        # -------------------- CLOCK PARAMETERS -------------------- #
+        self.active = None  # Boolean to determine if the clock is counting or not
+        self.session_count = None  # Number of completed sessions
+        self.FOCUS_DURATION = FOCUS_DURATION * 60  # Number of minutes to focus per cycle (in seconds)
+        self.BREAK_DURATION = BREAK_DURATION * 60  # Number of minutes for break per cycle (in seconds)
+        self.SESSIONS = NO_OF_SESSIONS  # Chosen default number of focus sessions
 
-        self.active = False  # Boolean to determine if the clock is counting or not
-        self.session_count = 0  # Number of completed sessions
-        self.FOCUS_DURATION = 25 * 60  # Number of minutes to focus per cycle (in seconds)
-        self.BREAK_DURATION = 5 * 60  # Number of minutes for break per cycle (in seconds)
-        self.SESSIONS = 4  # Chosen default number of focus sessions
         self.root.mainloop()  # Keep the window open (run the GUI)
 
     def start_timer(self):
         """Starts the timer"""
         self.active = True
+        self.session_count = 0
         self.start_button.config(state='disabled')  # Disable the start button because timer has started
-        self.stop_button.config(state='normal')  # Enable the stop button
-        self.run_sessions()
-
-    def stop_timer(self):
-        """Stops the timer from counting"""
-        self.active = False
-        self.start_button.config(state='normal')  # Enable the start button
-        self.stop_button.config(state='disable')  # Disable the stop button
+        self.reset_button.config(state='normal')  # Enable the stop button
+        self.run_sessions()  # Call the run sessions function
 
     def run_sessions(self):
         """Runs the focus and rest sessions of the timer"""
-        if self.session_count < self.SESSIONS:
-            self.session_count += 1  # Increase number of completed sessions by 1
-            self.timer_heading.config(text='Focus')  # Change the timer heading to display 'focus'
-            self.countdown(self.FOCUS_DURATION)
-            self.timer_heading.config(text='Break')  # Change the timer heading to display 'break'
-            self.countdown(self.BREAK_DURATION)
-            self.run_sessions()  # Run the next session
+        if self.active:
+            if self.session_count < self.SESSIONS:
+                self.session_count += 1  # Increase number of completed sessions by 1
+                # Update the session number in the gui
+                self.current_session.config(text='Session: {}/{}'.format(self.session_count, self.SESSIONS))
+                if self.session_count == self.SESSIONS:  # Final session
+                    self.timer_heading.config(text='Focus')  # Change the timer heading to display 'focus'
+                    self.countdown(self.FOCUS_DURATION)  # No break at the final session
+                    tkinter.messagebox.showinfo(title='Focus Timer', message='All sessions completed')
+                    self.reset_timer()  # Reset the timer
+                self.timer_heading.config(text='Focus')  # Change the timer heading to display 'focus'
+                self.countdown(self.FOCUS_DURATION)
+                self.timer_heading.config(text='Break')  # Change the timer heading to display 'break'
+                self.countdown(self.BREAK_DURATION)
+                self.run_sessions()  # Run the next session
         else:
-            self.session_count = 0
-            tkinter.messagebox.showinfo(title='Focus Timer', message='All sessions completed')
-            self.timer_heading.config(text='Timer')  # Return timer heading to display 'timer'
-            self.stop_timer()  # Stop the timer
+            self.reset_timer()
 
     def countdown(self, duration):
         """Initializes the countdown functionality for a specified duration"""
         while duration > 0 and self.active:
             minutes, seconds = divmod(duration, 60)  # Get the number of minutes and seconds
-            time_string = "{:02d}:{:02d}".format(minutes, seconds)  # The string for the countdown
+            time_string = "{:02d}:{:02d}".format(int(minutes), int(seconds))  # The string for the countdown
             self.canvas.itemconfig(self.time_text, text=time_string)
             self.root.update()  # Update the window to show countdown sequence
             time.sleep(1)  # Wait for a second
             duration -= 1  # Reduce the duration by a second after every second
 
+    def stop_countdown(self):
+        self.active = False  # Stop the clock from counting
+        self.timer_heading.config(text='Timer')  # Reset the heading of the clock
+        self.canvas.itemconfig(self.time_text, text='00:00')  # Replace time text with 00:00
+        self.current_session.config(text='Session: 0')  # Reset the number of sessions text
+
     def reset_timer(self):
-        self.stop_timer()
-        self.FOCUS_DURATION = 25 * 60
-        self.canvas.itemconfig(self.time_text, text='00:00')
-        self.timer_heading.config(text='Timer')
+        """Resets the timer and returns all setting to initial state"""
+        self.stop_countdown()  # Call the stop countdown function
         self.start_button.config(state='normal')
-        self.stop_button.config(state='disabled')
+        self.reset_button.config(state='disabled')  # Disable the reset button
